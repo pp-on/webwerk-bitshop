d89bc7678a00a26f90cb354a3bde219175766faa
Nickel Oswaldo <oswaldo.nickel@pfennigparade.de>
2024-08-05 15:40:45 +0200

    feat: add php to include template  according to cpt

---
+++ includes/single-cpt.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Single CPT Template
+ *Diese Datei wird die Logik enthalten, um zwischen den verschiedenen Custom Post Types zu unterscheiden und die entsprechenden Template-Dateien einzubinden.
+ * @package Webwerk Bitshop
+ */
+
+get_header(); 
+
+global $post;
+
+// Check the post type and include the appropriate template part
+if ( 'product' === $post->post_type ) {
+    get_template_part( 'template-parts/content', 'single-product' );
+} elseif ( 'magazine_cpt' === $post->post_type ) {
+    get_template_part( 'template-parts/content', 'single-magazine' );
+}
+
+get_footer();
+
